a719f183d04d0bb90a9df4df735f7c87a68d01d3
Gabriel Andreato <gabriel.andreato@minervafoods.com>
2024-11-18 18:14:23 -0300

    Implementado formulario para adicionar novo post

---
+++ src/features/posts/AddPostForm.tsx
@@ -0,0 +1,42 @@
+﻿import React, {FormEvent} from "react";
+import {useAppDispatch} from "@/app/hooks";
+
+interface AddPostFormFields extends HTMLFormControlsCollection {
+    postTitle: HTMLInputElement
+    postContent: HTMLTextAreaElement
+}
+
+interface AddPostFormElements extends HTMLFormElement {
+    readonly elements: AddPostFormFields;
+}
+
+export default function AddPostForm() {
+    const dispatch = useAppDispatch();
+
+    const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
+        e.preventDefault()
+
+        const {elements} = e.currentTarget;
+        const title = elements.postTitle.value;
+        const content = elements.postContent.value;
+
+        console.log('Values:', {title, content});
+
+        // dispatch(AddPostForm({title: title, content: content}))
+
+        e.currentTarget.reset();
+    }
+
+    return (
+        <section>
+            <h2>Add a New Post</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="postTitle" className="form-label">Title</label>
+                <input id="postTitle" type="text" title="postTitle"/>
+                <label htmlFor="postContent" className="form-label">Content</label>
+                <input id="postContent" type="text" title="postContent"/>
+                <button type="submit">Save</button>
+            </form>
+        </section>
+    )
+};
---
+++ src/features/posts/PostList.tsx
@@ -0,0 +1,20 @@
+﻿import {useAppSelector} from "@/app/hooks";
+import AddPostForm from "@/features/posts/AddPostForm";
+
+export function PostList() {
+    const posts = useAppSelector(state => state.posts);
+
+    const renderedPosts = posts.map(post => (
+        <article className="post-excerpt" key={post.id}>
+            <h3>{post.title}</h3>
+            <p className="post-content">{post.content.substring(0, 100)}</p>
+        </article>
+    ))
+
+    return (
+        <section>
+            <h2>Posts</h2>
+            {renderedPosts}
+        </section>
+    )
+}
